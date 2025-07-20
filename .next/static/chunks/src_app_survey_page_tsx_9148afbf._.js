d03cf3aeffd5d73397ecf440cc5e87abf7c7dd0a
Alifta Maulana Rayhan <32029337+raayend@users.noreply.github.com>
2025-07-20 23:54:21 +0700

    Add .next/static/chunks/src_app_survey_page_tsx_9148afbf._.js 1753030461146

---
+++ .next/static/chunks/src_app_survey_page_tsx_9148afbf._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_c760c4e1._.js",
+  "static/chunks/src_0120f651._.js"
+],
+    source: "dynamic"
+});
